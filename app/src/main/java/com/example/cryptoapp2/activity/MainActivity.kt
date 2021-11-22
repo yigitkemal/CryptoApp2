8148ca9a5d3f762a313d8a211c9a077d319cc653
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-11-22 12:37:16 +0300

    RxJava is added.

--- app/src/main/java/com/example/cryptoapp2/activity/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp2/activity/MainActivity.kt
@@ -10,10 +10,14 @@ import com.example.cryptoapp2.adapter.CryptoRecyclerViewAdapter
 import com.example.cryptoapp2.databinding.ActivityMainBinding
 import com.example.cryptoapp2.model.CryptoModel
 import com.example.cryptoapp2.service.CryptoAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +33,13 @@ class MainActivity : AppCompatActivity() {
     private var crytoModelsArrayList: ArrayList<CryptoModel>? = null
     private var cryptoAdapter = CryptoRecyclerViewAdapter(arrayListOf())
 
+    private var compositeDisposable: CompositeDisposable? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -40,52 +51,67 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = this.getResources().getColor(android.R.color.transparent)
         window.navigationBarColor = this.getResources().getColor(android.R.color.transparent)
 
+        compositeDisposable = CompositeDisposable()
+
         binding.recyclerViewMain.layoutManager = LinearLayoutManager(this)
 
-
         loadData()
-
     }
 
     private fun loadData() {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(CryptoAPI::class.java)
 
-        val call = retrofit.create(CryptoAPI::class.java).getData()
+        compositeDisposable?.add(
+            retrofit.getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse)
+        )
 
-        call.enqueue(object : Callback<List<CryptoModel>> {
-            override fun onResponse(
-                call: Call<List<CryptoModel>>,
-                response: Response<List<CryptoModel>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        crytoModelsArrayList = ArrayList(it)
 
-                        crytoModelsArrayList?.let {
-                            cryptoAdapter = CryptoRecyclerViewAdapter(it)
-                            binding.recyclerViewMain.adapter = cryptoAdapter
-                            cryptoAdapter.notifyDataSetChanged()
-                        }
+        /*
 
-                        /*for (cryptoModel: CryptoModel in crytoModelsArrayList!!) {
-                            println(cryptoModel.currency)
-                            println(cryptoModel.price)
+          val call = retrofit.create(CryptoAPI::class.java).getData()
 
-                        }*/
-                    }
-                }
-            }
+         call.enqueue(object : Callback<List<CryptoModel>> {
+              override fun onResponse(
+                  call: Call<List<CryptoModel>>,
+                  response: Response<List<CryptoModel>>)
+              {
+                  if (response.isSuccessful) {
+                      response.body()?.let {
+                          crytoModelsArrayList = ArrayList(it)
 
-            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
-                t.printStackTrace()
-            }
+                          crytoModelsArrayList?.let {
+                              cryptoAdapter = CryptoRecyclerViewAdapter(it)
+                              binding.recyclerViewMain.adapter = cryptoAdapter
+                              cryptoAdapter.notifyDataSetChanged()
+                          }
+                      }
+                  }
+              }
 
-        })
+              override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+                  t.printStackTrace()
+              }
+
+          }) */
 
 
     }
 
+    private fun handleResponse(cryptoList: List<CryptoModel>) {
+        crytoModelsArrayList = ArrayList(cryptoList)
+
+        crytoModelsArrayList?.let {
+            cryptoAdapter = CryptoRecyclerViewAdapter(it)
+            binding.recyclerViewMain.adapter = cryptoAdapter
+            cryptoAdapter.notifyDataSetChanged()
+        }
+
+    }
 }
--- app/src/main/java/com/example/cryptoapp2/service/CryptoAPI.kt
+++ app/src/main/java/com/example/cryptoapp2/service/CryptoAPI.kt
@@ -1,6 +1,7 @@
 package com.example.cryptoapp2.service
 
 import com.example.cryptoapp2.model.CryptoModel
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
@@ -8,6 +9,8 @@ import retrofit2.http.GET
 interface CryptoAPI {
 
     @GET("prices?key=fc673eb872161cda9a1e7639b8b31f43fa24f441")
-    open fun getData(): Call<List<CryptoModel>>
+    fun getData(): Observable<List<CryptoModel>>
+
+    //fun getData(): Call<List<CryptoModel>>
 
 }
